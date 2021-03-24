67d83866a5f50b35bd03c2d2d19f2433a0dd922f
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-03-24 09:45:38 +0100

    lektion 3

---
+++ Lektion-3/demo1/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8080;
+const serverURI = 'http://localhost:' + PORT;
+
+const middleware = (req, res, next) => {
+  console.log('running middleware');
+  next();
+}
+
+app.use('/posts', middleware);
+
+app.get('/', (req, res) => {
+  res.send('Homepage');
+})
+
+app.post('/posts', (req, res) => {
+  res.send('Post');
+})
+app.get('/posts', (req, res) => {
+  res.send('Get');
+})
+
+
+app.listen(PORT, () => console.log('Webserver: ' + serverURI));
