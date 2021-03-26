a5d72fefdce74816a39d2201d3d0a4e482e54e44
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-03-26 10:42:22 +0100

    lektion 4

---
+++ Lektion-4/1. products/models/products/productModel.js
@@ -0,0 +1,61 @@
+const mongodb = require('mongoose');
+const Product = require('./productSchema');
+
+
+exports.getProducts = (req, res) => {
+  Product.find()
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json(err))
+}
+
+exports.getOneProduct = (req, res) => {
+  Product.findOne({ _id: req.params.id })
+    .then(data => {
+      return res.status(200).json(data)
+    })
+    .catch(err => res.satus(500).json(err))
+}
+
+exports.createProduct = (req, res) => {
+
+  Product.find({ name: req.body.name })
+    .then(data => {
+      // console.log(data);
+
+      if(data.length > 0) {
+        return res.status(400).json({
+          statusCode: 400,
+          status: false,
+          message: 'A product by that name already exists, please update product instead'
+        })
+      }
+
+      
+
+      const product = new Product({
+        name: req.body.name,
+        short: req.body.short,
+        desc: req.body.desc,
+        price: req.body.price,
+        image: req.body.image
+      })
+
+      product.save()
+        .then(() => {
+          res.status(201).json({
+            statusCode: 201,
+            status: true,
+            message: 'Product created'
+          })
+        })
+        .catch(() => {
+          res.status(500).json({
+            statusCode: 500,
+            status: false,
+            message: 'Failed to create product'
+          })
+        })
+
+    })
+
+}
