bcfce0e0a897a264f4aa298dc499255d3f76de1d
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-04-07 14:48:06 +0200

    lektion 6

--- Lektion-6/3.event/src/App.vue
+++ Lektion-6/3.event/src/App.vue
@@ -1,16 +1,40 @@
 <template>
   <div>
     <hero />
+    <div class="container mt-5">
+
+      <div class="row">
+
+        <div class="col-6">
+          <sign-in />
+        </div>
+
+        <div class="col-6">
+          <sign-up/>
+        </div>
+
+        <!-- <div class="col-12">
+          <sign-in />
+        </div> -->
+
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
 import Hero from './components/Hero.vue'
-
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
 
 export default {
   name: 'App',
-  components: { Hero },
+  components: { 
+    Hero,
+    SignIn,
+    SignUp,
+  },
 
 }
 </script>
