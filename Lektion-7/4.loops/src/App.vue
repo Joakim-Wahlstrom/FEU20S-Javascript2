e1dbbb153ce7932a46e4d0ee33360e830722fd4e
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-04-09 13:42:37 +0200

    lektion 7

---
+++ Lektion-7/4.loops/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Names :names="names" />
+    <Loop :names="names" />
+  </div>
+</template>
+
+<script>
+import Names from './components/Names'
+import Loop from './components/Loop'
+
+export default {
+  name: 'App',
+  components: {
+    Names,
+    Loop
+  },
+  data() {
+    return {
+      names: ['Joakim', 'Jeanette', 'Hans', 'Tommy', 'Haitem', 'Ida', 'Joakim']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
