65966f07cd26d5eeb279ab8ab1a1abee334424a2
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-04-09 14:54:02 +0200

    lektion 7

--- Lektion-7/4.loops/src/App.vue
+++ Lektion-7/4.loops/src/App.vue
@@ -2,22 +2,33 @@
   <div>
     <Names :names="names" />
     <Loop :names="names" />
+    <Users :users="users" />
+    <Objects :users="users" />
   </div>
 </template>
 
 <script>
 import Names from './components/Names'
 import Loop from './components/Loop'
+import Users from './components/Users'
+import Objects from './components/Objects'
 
 export default {
   name: 'App',
   components: {
     Names,
-    Loop
+    Loop,
+    Users,
+    Objects
   },
   data() {
     return {
-      names: ['Joakim', 'Jeanette', 'Hans', 'Tommy', 'Haitem', 'Ida', 'Joakim']
+      names: ['Joakim', 'Jeanette', 'Hans', 'Tommy', 'Haitem', 'Ida', 'Joakim'],
+      users: [
+        {id: 1, name: 'Joakim', age: 34},
+        {id: 2, name: 'Jeanette', age: 34},
+        {id: 3, name: 'Hans', age: 36}
+      ]
     }
   }
 }
---
+++ Lektion-7/4.loops/src/components/Users.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="users.length">
+    <User v-for="user in users" :key="user.id" :user="user" />
+  </div>
+  <div v-else>
+    <h3>No Users registrated</h3>
+  </div>
+</template>
+
+<script>
+import User from './User'
+export default {
+  props: ['users'],
+  components: {
+    User
+  }
+}
+</script>
+
+<style>
+
+</style>
