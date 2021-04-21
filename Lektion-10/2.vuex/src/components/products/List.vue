3096ccbc80308617bafdae1591200cf87c8b6459
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-04-21 14:42:56 +0200

    lektion 10

--- Lektion-10/2.vuex/src/components/products/List.vue
+++ Lektion-10/2.vuex/src/components/products/List.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <button class="btn">ADD</button>
-    <button class="btn">SUB</button>
+    <button class="btn" @click="addToPrice">ADD</button>
+    <button class="btn" @click="subPrice(5)">SUB</button>
     <div class="list">
-      <ProductCard v-for="product in products" :key="product.id" class="card" :product="product" />
+      <ProductCard v-for="product in prodTax" :key="product.id" class="card" :product="product" />
     </div>
   </div>
 </template>
@@ -16,23 +16,51 @@ export default {
   },
   data() {
     return {
-      products: [
-        {id: 1, name: 'Product 1', price: 200},
-        {id: 2, name: 'Product 2', price: 299},
-        {id: 3, name: 'Product 3', price: 500},
-        {id: 4, name: 'Product 4', price: 100},
-        {id: 5, name: 'Product 5', price: 234},
-        {id: 6, name: 'Product 6', price: 999},
-        {id: 7, name: 'Product 7', price: 222},
-        {id: 8, name: 'Product 8', price: 200},
-        {id: 9, name: 'Product 9', price: 300},
-        {id: 10, name: 'Product 10', price: 400},
-        {id: 11, name: 'Product 11', price: 500},
-        {id: 12, name: 'Product 12', price: 600},
-        {id: 13, name: 'Product 13', price: 700},
-        {id: 14, name: 'Product 14', price: 800},
-        {id: 15, name: 'Product 15', price: 900},
-      ]
+      // products: [
+      //   {id: 1, name: 'Product 1', price: 200},
+      //   {id: 2, name: 'Product 2', price: 299},
+      //   {id: 3, name: 'Product 3', price: 500},
+      //   {id: 4, name: 'Product 4', price: 100},
+      //   {id: 5, name: 'Product 5', price: 234},
+      //   {id: 6, name: 'Product 6', price: 999},
+      //   {id: 7, name: 'Product 7', price: 222},
+      //   {id: 8, name: 'Product 8', price: 200},
+      //   {id: 9, name: 'Product 9', price: 300},
+      //   {id: 10, name: 'Product 10', price: 400},
+      //   {id: 11, name: 'Product 11', price: 500},
+      //   {id: 12, name: 'Product 12', price: 600},
+      //   {id: 13, name: 'Product 13', price: 700},
+      //   {id: 14, name: 'Product 14', price: 800},
+      //   {id: 15, name: 'Product 15', price: 900}
+      // ]
+    }
+  },
+  methods: {
+    addToPrice() {
+      // this.$store.state.products.forEach(product => {
+      //   product.price += 10
+      // })
+      this.$store.commit('ADD', 1)
+    },
+    subPrice(amount) {
+      // this.$store.commit('SUB_ASYNC', amount)
+      this.$store.dispatch('subAsync', amount)
+    }
+  },
+  computed: {
+    products() {
+      return this.$store.state.products
+    },
+    prodTax() {
+      // let taxedProducts = this.$store.state.products.map(product => {
+      //   return {
+      //     id: product.id,
+      //     name: product.name + ' + tax',
+      //     price: Math.round(product.price + product.price * 0.2)
+      //   }
+      // })
+      // return taxedProducts
+      return this.$store.getters.taxedProducts
     }
   }
 }
--- Lektion-10/2.vuex/src/store/index.js
+++ Lektion-10/2.vuex/src/store/index.js
@@ -5,10 +5,66 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    products: [
+      {id: 1, name: 'Product 1', price: 200},
+      {id: 2, name: 'Product 2', price: 299},
+      {id: 3, name: 'Product 3', price: 500},
+      {id: 4, name: 'Product 4', price: 100},
+      {id: 5, name: 'Product 5', price: 234},
+      {id: 6, name: 'Product 6', price: 999},
+      {id: 7, name: 'Product 7', price: 222},
+      {id: 8, name: 'Product 8', price: 200},
+      {id: 9, name: 'Product 9', price: 300},
+      {id: 10, name: 'Product 10', price: 400},
+      {id: 11, name: 'Product 11', price: 500},
+      {id: 12, name: 'Product 12', price: 600},
+      {id: 13, name: 'Product 13', price: 700},
+      {id: 14, name: 'Product 14', price: 800},
+      {id: 15, name: 'Product 15', price: 900}
+    ]
+  },
+  getters: {
+    taxedProducts: state => {
+      let taxedProducts = state.products.map(product => {
+        return {
+          id: product.id,
+          name: product.name + ' + tax',
+          price: Math.round(product.price + product.price * 0.2)
+        }
+      })
+      return taxedProducts
+    }
   },
   mutations: {
+    ADD: (state, amount) => {
+      state.products.forEach(product => {
+        product.price += amount
+      })
+    },
+    // SUB_ASYNC: (state, amount) => {
+    //   setTimeout(() => {
+    //     state.products.forEach(product => {
+    //       product.price -= amount
+    //     })
+    //   }, 3000)
+    // }
+    SUB: (state, amount) => {
+      state.products.forEach(product => {
+        product.price -= amount
+      })
+    }
   },
   actions: {
+    // subAsync: (context, amount) => {
+    //   setTimeout(() => {
+    //     context.commit('SUB', amount)
+    //   }, 4000)
+    // },
+    subAsync: ({commit}, amount) => {
+      setTimeout(() => {
+        commit('SUB', amount)
+      }, 4000)
+    }
   },
   modules: {
   }
