2dc3907522cca9a5a7b3c52bde7bf82322c4c4f3
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-04-21 09:41:39 +0200

    lektion 10

---
+++ Lektion-10/1.routernavigation/src/views/Integrity.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Integrity Policy</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet, iusto, consequuntur architecto obcaecati animi nam asperiores magni natus dicta possimus suscipit id nesciunt necessitatibus neque enim quos recusandae quod eum.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ Lektion-9/4.router/src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav>
+    <h1>LOGO</h1>
+    <ul class="d-flex">
+      <li><router-link to="/" exact class="link">Home</router-link></li>
+      <li><router-link to="/about" exact class="link">About</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 1rem;
+    background: #002222;
+    color: #fff;
+  }
+  li {
+    list-style: none;
+  }
+  .link {
+    text-decoration: none;
+    color: #fff;
+    padding: 1rem;
+    display: inline-block;
+  }
+  .link:hover {
+    background: rgba(0, 0, 0, 0.233);
+  }
+  .router-link-active {
+    text-decoration: underline;
+  }
+  /* .router-link-exact-active {
+    text-decoration: underline;
+  } */
+</style>
