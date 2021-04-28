66fe233ada046a84a453dd49068544b876e7d45a
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-04-28 10:58:21 +0200

    lektion 12

--- Lektion-12/shop/src/views/Products.vue
+++ Lektion-12/shop/src/views/Products.vue
@@ -4,9 +4,16 @@
 
 <script>
 import ProductCardDeck from '../components/products/ProductCardDeck'
+import { mapActions } from 'vuex'
 export default {
   components: {
     ProductCardDeck
+  },
+  methods: {
+    ...mapActions(['getProducts'])
+  },
+  created() {
+    this.getProducts()
   }
 }
 </script>
